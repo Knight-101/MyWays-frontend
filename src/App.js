e5effde1dcbee04d8cde615bfdb47761ffb86b62
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2021-07-31 21:20:41 +0530

    frontend complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import "./App.css";
-// import AuthRoute from "./components/AuthRoute";
 import BlogPage from "./components/BlogPage/BlogPage";
 
 function App() {
